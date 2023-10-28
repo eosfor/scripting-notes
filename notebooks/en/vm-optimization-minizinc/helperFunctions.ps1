03f64c3cb629db4f85f8998e9041528f3a99f91d
= <=>
2023-10-27 17:08:11 -0700

    updated model

--- notebooks/en/vm-optimization-minizinc/helperFunctions.ps1
+++ notebooks/en/vm-optimization-minizinc/helperFunctions.ps1
@@ -5,102 +5,36 @@ else {
     $rootFolderPath = "./vm-optimization-minizinc"
 }
 
-
-
-function New-TempModelFile {
-    [CmdletBinding()]
-    param (
-        $SourceModelPath = "$rootFolderPath\vmCostsCalculation-integer.mzn"
-    )
-    process {
-        $tmpFile = New-TemporaryFile
-        $newName = "$($tmpFile.BaseName).mzn"
-        $newPath = Join-Path -Path $tmpFile.DirectoryName -ChildPath $newName
-        Rename-Item -Path $tmpFile.FullName -NewName $newName
-
-        Get-Content -Path $SourceModelPath -ReadCount 0 | Out-File $newPath -Force
-        $newPath
-    }    
-}
-
-
-function Invoke-Minizinc {
-    [CmdletBinding()]
-    param (
-        $Solver = "gecode",
-        $Modelpath,
-        $DataPath,
-        $TimeLimit = (10 * 60 * 1000)
-    )   
-    process {
-        if (-not (Test-Path $Modelpath) ) {Write-Error "$Modelpath does not exist"}
-        
-        Write-Verbose "Invoking: minizinc --solver $Solver  $Modelpath $DataPath --time-limit (10 * 60 * 1000)"
-        
-        if ([System.Environment]::OSVersion.Platform -eq "Win32NT") {
-            $k = minizinc.exe --solver $Solver  $Modelpath $DataPath --time-limit (10 * 60 * 1000)
-        }
-        else {
-            $k = minizinc --solver $Solver  $Modelpath $DataPath --time-limit (10 * 60 * 1000)
-        }
-
-        
-        $x = ((($k) -replace "==========","") -join "`n" -split "----------") | ConvertFrom-Json -Depth 10
-
-        Write-Verbose "modelling res is: $x"
-
-        $x | Where-Object {$_ -ne $null}
-    }
-}
-
 function Start-MinizincVMOptimizationModel {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'MinimizeCosts')]
     param (
-        [Parameter(ParameterSetName = "Costs", Mandatory)]
-        [switch]$Costs,
-        [Parameter(ParameterSetName = "Performance", Mandatory)]
-        [switch]$Performance,
-        
-        [Parameter(ParameterSetName = "Performance")]
-        [Parameter(ParameterSetName = "Costs")]
-        [string]$DataPath = "$rootFolderPath\vmData-integer.dzn",
-
-        [Parameter(ParameterSetName = "Performance", ValueFromPipeline = $true)]
-        [Parameter(ParameterSetName = "Costs", ValueFromPipeline = $true)]
+        [Parameter(ParameterSetName = "MinimizeCosts", Mandatory)]
+        [switch]$MinimizeCosts,
+        [Parameter(ParameterSetName = "MaximizeACU", Mandatory)]
+        [switch]$MaximizeACU,
+        [Parameter(ParameterSetName = "FixCostsMaximizeACU", Mandatory)]
+        [switch]$FixCostsMaximizeACU,
+        [Parameter(ParameterSetName = "FixACUMinimizeCosts", Mandatory)]
+        [switch]$FixACUMinimizeCosts,
+        [Parameter(ParameterSetName = "FixCostsMaximizeACU", ValueFromPipeline = $true)]
+        [Parameter(ParameterSetName = "FixACUMinimizeCosts", ValueFromPipeline = $true)]
         $InputObject = $null
     )
+    begin {
+        $maximizeTotalACU = 'vm-optimization-minizinc/maximize-totalACU.mzn'
+        $minimizeTotalPrice = 'vm-optimization-minizinc/minimize-totalPrice.mzn'
+        $fixTotalACU = 'vm-optimization-minizinc/fix-totalACU.mzn'
+        $fixTotalPrice = 'vm-optimization-minizinc/fix-totalPrice.mzn'
+        $DataPath = "vm-optimization-minizinc/vmData-integer.dzn"
+    }
     process {
-        $tFile = New-TempModelFile
-
-        if (-not (Test-Path $DataPath) ) {Write-Error "$DataPath does not exist"}
-
-        if($Costs.IsPresent) {
-            if ($null -eq $InputObject) {
-                (Get-Content $tFile) -replace "%placeholder%", "solve  minimize totalPrice;" | out-file -FilePath $tFile -Force
-            }
-            else {
-                (Get-Content $tFile) -replace "%placeholder%", "constraint totalACU >= $($InputObject.totalACU); solve  minimize totalPrice;" | out-file -FilePath $tFile -Force
-            }
-
-            $ret = Invoke-Minizinc -Modelpath $tFile -DataPath (Resolve-Path $DataPath).Path
-            return $ret
-
+        switch ($PSCmdlet.ParameterSetName) {
+            'MinimizeCosts' { Invoke-Minizinc -ModelPath $minimizeTotalPrice -DataPath $DataPath; break }
+            'MaximizeACU' { Invoke-Minizinc -ModelPath $maximizeTotalACU -DataPath $DataPath; break }
+            'FixCostsMaximizeACU' { Invoke-Minizinc -ModelPath $fixTotalPrice -DataPath $DataPath -Data "totalPriceMax=$($InputObject.totalPrice*10000)"; break }
+            'FixACUMinimizeCosts' { Invoke-Minizinc -ModelPath $fixTotalACU -DataPath $DataPath -Data "totalAcuMax=$($InputObject.totalACU)"; break}
+            Default {}
         }
-
-        if ($Performance.IsPresent){
-            if ($null -eq $InputObject){
-                (Get-Content $tFile) -replace "%placeholder%", "solve  maximize totalACU;" | out-file -FilePath $tFile -Force
-            }
-            else {
-                (Get-Content $tFile) -replace "%placeholder%", "constraint totalPrice <= $($InputObject.totalPrice * 10000); solve  maximize totalACU;" | out-file -FilePath $tFile -Force
-            }
-            
-            $ret = Invoke-Minizinc -Modelpath $tFile -DataPath (Resolve-Path $DataPath).Path
-            return $ret
-        }        
+        
     }
 }
-
-# Start-MinizincVMOptimizationModel -Costs
-
-# Start-MinizincVMOptimizationModel -Costs | Start-MinizincVMOptimizationModel -Performance
