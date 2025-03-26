c2ec253d3e3e937cb698e6a1c39fd1da551600a1
eosfor <9363027+eosfor@users.noreply.github.com>
2025-03-26 04:31:00 +0000

    chnage the way we make devcontainer

---
+++ .devcontainer/container-build.ps1
@@ -0,0 +1,18 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y minizinc
+
+# Install PowerShell dependencies
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue;
+if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction Ignore)) {
+    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
+}
+if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.2.1 -ErrorAction Ignore)) {
+    Install-Module PowerShellGet -MinimumVersion 2.2.1 -Scope CurrentUser -Force -AllowClobber;
+}
+if ($Null -eq (Get-InstalledModule -Name InvokeBuild -MinimumVersion 5.4.0 -ErrorAction Ignore)) {
+    Install-Module InvokeBuild -MinimumVersion 5.4.0 -Scope CurrentUser -Force;
+}
+if ($Null -eq (Get-InstalledModule -Name PSRule.Rules.Azure  -ErrorAction Ignore)) {
+    Install-Module PSRule.Rules.Azure -Scope CurrentUser -Force;
+}
