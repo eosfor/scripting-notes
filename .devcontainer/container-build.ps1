0c7a619f09226c0393d4db8720b298b7b0372971
eosfor <9363027+eosfor@users.noreply.github.com>
2025-04-20 18:28:39 -0700

    - psrule-related tests. it is in a different repo

--- .devcontainer/container-build.ps1
+++ .devcontainer/container-build.ps1
@@ -7,12 +7,3 @@ $ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyC
 if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction Ignore)) {
     Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
 }
-if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.2.1 -ErrorAction Ignore)) {
-    Install-Module PowerShellGet -MinimumVersion 2.2.1 -Scope CurrentUser -Force -AllowClobber;
-}
-if ($Null -eq (Get-InstalledModule -Name InvokeBuild -MinimumVersion 5.4.0 -ErrorAction Ignore)) {
-    Install-Module InvokeBuild -MinimumVersion 5.4.0 -Scope CurrentUser -Force;
-}
-if ($Null -eq (Get-InstalledModule -Name PSRule.Rules.Azure  -ErrorAction Ignore)) {
-    Install-Module PSRule.Rules.Azure -Scope CurrentUser -Force;
-}
